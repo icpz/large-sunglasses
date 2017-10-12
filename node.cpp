ed432663b78ec21bbd91eaa2e8752eda5b8f8c71
ier005 <ier005@users.noreply.github.com>
2017-10-12 11:04:08 +0800

    user related

--- node.cpp
+++ node.cpp
@@ -1,38 +1,42 @@
-#include "node.h"
+﻿#include "node.h"
 
-Node::Node()
+Node::Node() : registered(false), user_found(false)
 {
-
+    connect(this, &Node::sig_registered, this, &Node::get_friends, Qt::DirectConnection);
 }
 
 Node::~Node()
 {
-    delete identity;
+    delete node;
 }
 
 int Node::signup(QString pw)
 {
-    auto key = std::make_shared<dht::crypto::PrivateKey>(dht::crypto::PrivateKey::generateEC());
-    auto cert = std::make_shared<dht::crypto::Certificate>(dht::crypto::Certificate::generate(*key));
-    dht::crypto::Identity iden = {std::move(key), std::move(cert)};
+    auto key = dht::crypto::PrivateKey::generateEC();
+    std::vector<uint8_t> pk_s = key.serialize(pw.toStdString());
 
-    int len = sizeof(iden);
-    std::vector<uint8_t> data;
-    data.resize(len);
-    memcpy(&data[0], &iden, len);
-    std::vector<uint8_t> crypted = dht::crypto::aesEncrypt(data, pw.toStdString());
-    QFile file("identity");
+    auto cert = dht::crypto::Certificate::generate(key);
+    std::vector<uint8_t> cert_s;
+    cert.pack(cert_s);
+
+    QFile file("privkey");
     if (!file.open(QIODevice::WriteOnly)) {
         return 0;
     }
-    file.write((char *)&crypted[0], crypted.size());
+    file.write((char *)&pk_s[0], pk_s.size());
     file.close();
+    QFile file1("cert");
+    if (!file1.open(QIODevice::WriteOnly)) {
+        return 0;
+    }
+    file1.write ((char *)&cert_s[0], cert_s.size());
+    file1.close();
     return 1;
 }
 
 int Node::checkid(QString pw)
 {
-    QFile file("identity");
+    QFile file("cert");
     if (!file.open(QIODevice::ReadOnly)) {
         return 0;
     }
@@ -40,15 +44,126 @@ int Node::checkid(QString pw)
     std::vector<uint8_t> data;
     data.resize(bytes.length());
     memcpy(&data[0], bytes.data(), bytes.length());
+    this->cert = std::make_shared<dht::crypto::Certificate>(data);
+    file.close();
+    QFile file1("privkey");
+    if (!file1.open(QIODevice::ReadOnly)) {
+        return 0;
+    }
+    bytes = file1.readAll();
+    file1.close();
+    data.resize(bytes.length());
+    memcpy(&data[0], bytes.data(), bytes.length());
     try {
-        std::vector<uint8_t> dec = dht::crypto::aesDecrypt(data, pw.toStdString());
-        char *ptr = static_cast<char *>(malloc(sizeof(dht::crypto::Identity)));
-        for (int i = 0; i < sizeof(dht::crypto::Identity); i++) {
-            ptr[i] = dec[i];
-        }
-        this->identity = reinterpret_cast<dht::crypto::Identity *>(ptr);
+        this->pk = std::make_shared<dht::crypto::PrivateKey>(data, pw.toStdString());
         return 1;
     } catch(std::exception) {
         return 0;
     }
+}
+
+
+void Node::conn()
+{
+    node = new dht::DhtRunner();
+
+    node->run(4222, dht::crypto::Identity(this->pk, this->cert), true);
+    node->bootstrap(BOOTSTRAP, "4222");
+    id = node->getId().toString();
+    qDebug("%s", id.data());
+
+    node->get("key_users",
+              [this](const std::vector<std::shared_ptr<dht::Value>>& values) {
+                  int found = 0;
+                  for (const auto &value : values) {
+                      if (value->data.size() != this->id.size()) {
+                          continue;
+                      }
+                      int i;
+                      for (i = 0; i < this->id.size() && value->data[i] == this->id[i]; i++);
+                      if (i == this->id.size()) {
+                          found = 1;
+                          break;
+                      }
+                  }
+                  if (found) {
+                      this->registered = true;
+                      return false;
+                  }
+
+                  return true;
+              },
+              [this](bool) {
+                  if (this->registered) {
+                      emit this->sig_registered(true);
+                  } else {
+                      emit this->sig_registered(false);
+                  }
+    });
+
+}
+
+void Node::get_friends(bool b)
+{
+    if (!b) {
+        node->put("key_users", dht::Value(4, reinterpret_cast<const uint8_t*>(this->id.c_str()), this->id.size()), {}, {}, true);
+    } else {
+        node->get("key_friends",
+                  [this](const std::vector<std::shared_ptr<dht::Value>>& values) {
+                      for (const auto &value : values) {
+                          int i;
+                          for (i = 0; i < 40 && value->data[i] == this->id[i]; i++);
+                          if (i == 40) {
+                              this->friends.insert(friends.end(), std::string(value->data.begin() + 40, value->data.end()));
+                          }
+                          for (i = 40; i < 80 && value->data[i] == this->id[i - 40]; i++);
+                          if (i == 80) {
+                              this->friends.insert(friends.end(), std::string(value->data.begin(), value->data.begin() + 40));
+                          }
+                      }
+                      return true;
+                  },
+            [this](bool) {
+                emit sig_friends();
+            }
+        );
+    }
+}
+
+void Node::search_user(QString uid)
+{
+    node->get("key_users",
+              [&uid, this](const std::vector<std::shared_ptr<dht::Value>>& values) {
+                  int found = 0;
+                  for (const auto &value : values) {
+                      if (value->data.size() != uid.toStdString().size()) {
+                          continue;
+                      }
+                      int i;
+                      for (i = 0; i < this->id.size() && value->data[i] == uid.toStdString()[i]; i++);
+                      if (i == this->id.size()) {
+                          found = 1;
+                          break;
+                      }
+                  }
+                  if (found) {
+                      this->user_found = true;
+                      return false;
+                  }
+
+                  return true;
+              },
+              [this](bool) {
+                  if (this->user_found) {
+                      this->user_found = false;
+                      emit this->sig_user_found(true);
+                  } else {
+                      emit this->sig_user_found(false);
+                  }
+    });
+}
+
+void Node::add_friend(QString uid)
+{
+    //node->put();
 }
--- node.h
+++ node.h
@@ -4,18 +4,39 @@
 #include <opendht.h>
 #include <QString>
 #include <QFile>
-#include <QDebug>
+#include <QObject>
 
-class Node
+#define BOOTSTRAP "192.168.98.194"
+
+class Node: public QObject
 {
+    Q_OBJECT
+
 public:
     Node();
     ~Node();
     static int signup(QString);
     int checkid(QString);
+    void conn();
+    void search_user(QString);
+    void add_friend(QString);
 
 private:
-    dht::crypto::Identity *identity;
+    dht::DhtRunner *node;
+    std::string id;
+    std::shared_ptr<dht::crypto::PrivateKey> pk;
+    std::shared_ptr<dht::crypto::Certificate> cert;
+    bool registered;
+    bool user_found;
+    std::vector<std::string> friends;
+
+signals:
+    void sig_registered(bool);
+    void sig_friends();
+    void sig_user_found(bool);
+
+private slots:
+    void get_friends(bool);
 };
 
 #endif // NODE_H
